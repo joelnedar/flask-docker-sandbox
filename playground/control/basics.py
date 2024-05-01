6c74ed48efbf2bc7fc55b7240c299d3e9b370545
Joel Nedar <joelnedar@gmail.com>
2024-05-01 14:55:43 +0200

    Add new route for onsdagsklubbens names

--- playground/control/basics.py
+++ playground/control/basics.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3.6.3
-from flask import jsonify
 from playground import app
 import random
 
 """ ********* HEALTH CHECKS ********* """
 
-
 @app.route("/")
 def hello_check():
     greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
-    return jsonify(random.choice(greeting_list) + " World")
+    return random.choice(greeting_list) + " World"
+
+@app.route("/names")
+def hello_check_name():
+    hello_world = hello_check()
+    name_list = ['Jenny', 'Linnéa', 'Louise', 'Mikaela']
+    return hello_world + ", " + random.choice(name_list)
